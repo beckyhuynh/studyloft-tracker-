2ad084e0f71a8d641279b734fd1d1d581aa750cf
Becky <beckyhuynh12@gmail.com>
2026-05-18 21:37:13 -0700

    logic for moving the furniture around

--- studycafe/frontend/src/Room/Room.tsx
+++ studycafe/frontend/src/Room/Room.tsx
@@ -1,9 +1,53 @@
 import './Room.css'
 import Tile from '../Tile/Tile.js';
+import React from 'react';
 
 // for 8 x 8 tile room
 const horizontalAxis = ["a","b","c","d","e","f","g","h"]
 const verticalAxis = ["1","2","3","4","5","6","7","8"]
+
+interface Piece{
+    image: string
+    x: number
+    y: number
+}
+
+const pieces: Piece[] = [];
+
+pieces.push({image: "./images/assets/chair.png", x:0, y:7})
+pieces.push({image: "./images/assets/table.png", x:1, y:5})
+
+let activePiece: HTMLElement | null = null;
+
+function grabItem(e: React.MouseEvent){
+    const element = e.target as HTMLElement;
+    if(element.classList.contains("itempiece")) {
+        const x = e.clientX - 80;
+        const y = e.clientY - 80;
+        element.style.position = "absolute";
+        element.style.left = `${x}px`
+        element.style.top = `${y}px`;
+
+        activePiece = element; // only set if we clicked onto it
+    }
+
+}
+
+function movePiece(e: React.MouseEvent) {
+    if (activePiece) {
+        const x = e.clientX - 80;
+        const y = e.clientY - 80;
+        activePiece.style.position = "absolute";
+        activePiece.style.left = `${x}px`
+        activePiece.style.top = `${y}px`;
+    }
+}
+
+function dropPiece(e: React.MouseEvent) {
+    if(activePiece) {
+        activePiece = null;
+    }
+}
 
 function Room(){
     // display inventory items in a scrollable horizontal list at top
@@ -15,22 +59,27 @@ function Room(){
     for (let j = verticalAxis.length - 1; j>=0;j--) {
         for(let i = 0; i < horizontalAxis.length; i++) {
             const number = j + i + 2;
+            let image = undefined;
 
-            if (number % 2 == 0) {
-                //floor.push(<span className = "tile light-tile">[{horizontalAxis[i]}{verticalAxis[j]}]</span>)
-                floor.push(<Tile/>)
-            }
+            pieces.forEach(p => {
+                if (p.x == i && p.y == j) {
+                    image = p.image;
+                }
+            })
 
-            else{
-                // floor.push(
-                // <span className = "tile dark-tile">[{horizontalAxis[i]}{verticalAxis[j]}]</span>
-                // )
-                floor.push(<Tile/>)
-            }
+            floor.push(<Tile key = {`${j},${i}`} image = {image} number = {number} />);
         }
     }
 
-    return <div className = "emptyroom">{floor}</div>;
+    return (
+    <div 
+    onMouseMove = {(e) => movePiece(e)} 
+    onMouseDown={e => grabItem(e)} 
+    onMouseUp = {(e) => dropPiece(e)}
+    className = "emptyroom">
+        {floor}
+    </div>
+    );
 }
 
 export default Room;
--- studycafe/frontend/src/Tile/Tile.tsx
+++ studycafe/frontend/src/Tile/Tile.tsx
@@ -1,9 +1,24 @@
 import "./Tile.css"
 
 interface Props{
-    number:number;
+    image?: string;
+    number: number;
 }
 
-export default function Tile(){
-    return <div className = "tile"></div>
+export default function Tile({number, image}:Props){
+    if (number % 2 == 0) {
+        return (
+        <div className = "tile dark-tile" >
+           {image && <div style = {{backgroundImage:`url(${image})`}} className = "itempiece"></div>}
+        </div>
+        );
+    }
+
+    else{
+        return (
+        <div className = "tile light-tile" >
+            {image && <div style = {{backgroundImage:`url(${image})`}} className = "itempiece"></div>}
+        </div>
+        );
+    }
 }
