69fb49b4c386f962ca1e616f1b03cd1ee462b17e
Becky <beckyhuynh12@gmail.com>
2026-05-19 22:31:29 -0700

    snapping furniture to floor slot

--- studycafe/frontend/src/Room/Room.tsx
+++ studycafe/frontend/src/Room/Room.tsx
@@ -1,7 +1,7 @@
 import './Room.css'
 import Tile from '../Tile/Tile.js';
 import React from 'react';
-import {useRef, useState} from "react";
+import {useRef, useState, useEffect} from "react";
 
 // for 8 x 8 tile room
 const horizontalAxis = ["a","b","c","d","e","f","g","h"]
@@ -13,32 +13,39 @@ interface Piece{
     y: number
 }
 
-const pieces: Piece[] = [];
+const initialFloorState: Piece[] = [];
 
-pieces.push({image: "./images/assets/chair.png", x:0, y:7})
-pieces.push({image: "./images/assets/table.png", x:1, y:5})
+initialFloorState.push({image: "./images/assets/chair.png", x:0, y:7})
+initialFloorState.push({image: "./images/assets/table.png", x:1, y:5})
 
 
 function Room(){
-    //const [pieces, setPieces] = useState<Piece[]>()
+    const [activePiece, setActivePiece] = useState<HTMLElement | null>(null);
+    const [gridX, setgridX] = useState(0);
+    const [gridY, setgridY] = useState(0);
+    const [pieces, setPieces] = useState<Piece[]>(initialFloorState)
     // display inventory items in a scrollable horizontal list at top
     // drag items to a tile in the room, update locations, store inside database
     // when the server reloads again, it should render the entire room with item in same spot
     // can move item to different location in the room
     const roomRef = useRef<HTMLDivElement>(null);
 
-    let activePiece: HTMLElement | null = null;
+    // let activePiece: HTMLElement | null = null;
 
     function grabItem(e: React.MouseEvent){
         const element = e.target as HTMLElement;
-        if(element.classList.contains("itempiece")) {
+        const room = roomRef.current;
+        if(element.classList.contains("itempiece") && room) {
+            setgridX(Math.floor((e.clientX- room.offsetLeft) / 150));
+            setgridY(Math.abs(Math.ceil((e.clientY - room.offsetTop - 1200) / 150)));
             const x = e.clientX - 80;
             const y = e.clientY - 80;
             element.style.position = "absolute";
             element.style.left = `${x}px`
             element.style.top = `${y}px`;
 
-            activePiece = element; // only set if we clicked onto it
+            setActivePiece(element);
+            // activePiece = element; // only set if we clicked onto it
         }
 
     }
@@ -77,9 +84,23 @@ function Room(){
     }
 
     function dropPiece(e: React.MouseEvent) {
-        if(activePiece) {
-            // pieces[0].x = 5;
-            activePiece = null;
+        const room = roomRef.current;
+        if(activePiece && room) {
+            const x = Math.floor((e.clientX- room.offsetLeft) / 150);
+            const y = Math.abs(Math.ceil((e.clientY - room.offsetTop - 1200) / 150));
+            //console.log(x,y);
+
+            setPieces((value) => {
+                const pieces = value.map(p => {
+                    if (p.x == gridX && p.y == gridY) {
+                        p.x = x;
+                        p.y = y;
+                    }
+                    return p;
+                })
+                return pieces;
+            });
+            setActivePiece(null);
         }
     }
 
