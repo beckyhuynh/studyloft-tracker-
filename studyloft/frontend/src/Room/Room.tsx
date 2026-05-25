9add45ddba626fa11b88104eb1de85141bba9afc
Becky <beckyhuynh12@gmail.com>
2026-05-24 22:42:41 -0700

    begin fixing count display in decor tab

--- studyloft/frontend/src/Room/Room.tsx
+++ studyloft/frontend/src/Room/Room.tsx
@@ -1,207 +1,226 @@
 import './Room.css'
 import Tile from '../Tile/Tile.js';
-import React from 'react';
+import React, { JSX } from 'react';
 import {useRef, useState, useEffect} from "react";
 import '../Bar.css'
 
-// for 8 x 8 tile room
-const horizontalAxis = ["a","b","c","d","e","f","g","h"]
-const verticalAxis = ["1","2","3","4","5","6","7","8"]
-
-const barList = [];
-
-const chair = ["./images/assets/chairFront.png","./images/assets/chairLeft.png", "./images/assets/chairBack.png", "./images/assets/chairRight.png"];
-
-const furniture: Array<string>[] = [];
-furniture.push(chair);
-
-// pushing from cart into the inventoryBar
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src = {chair[0]}/>
-        <div className = "imageText">10</div>
-    </div>
-    );
-
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/table.png"/>
-        <div className = "imageText">10</div>
-    </div>
-    );
-
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/toilet.png"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/oven.png"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/plant.png"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/window.png"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/painting1.png"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/painting2.png"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/dog.gif"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/cat.gif"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/couchLeft.png"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/couchRight.png"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-    barList.push(
-<div className = "container">
-    <img className = "picture" style = {{width:150, height:150}} src="./images/assets/fridgeTop.png"/>
-    <div className = "imageText">10</div>
-</div>
-    );
-
-barList.push(
-<div className = "container">
-    <img className = "picture" style = {{width:150, height:150}} src="./images/assets/fridgeBottom.png"/>
-    <div className = "imageText">10</div>
-</div>
-    );
-
-barList.push(
-<div className = "container">
-    <img className = "picture" style = {{width:150, height:150}} src="./images/assets/sink.png"/>
-    <div className = "imageText">10</div>
-</div>
-    );
-
-barList.push(
-<div className = "container">
-    <img className = "picture" style = {{width:150, height:150}} src="./images/assets/toiletSide.png"/>
-    <div className = "imageText">10</div>
-</div>
-    );
-
-barList.push(
-<div className = "container">
-    <img className = "picture" style = {{width:150, height:150}} src="./images/assets/bathTubLeft.png"/>
-    <div className = "imageText">10</div>
-</div>
-    );
-
-barList.push(
-<div className = "container">
-    <img className = "picture" style = {{width:150, height:150}} src="./images/assets/bathTubRight.png"/>
-    <div className = "imageText">10</div>
-</div>
-    );
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/clock.png"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/carpet.png"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/desk.png"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/bedBottom.png"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-barList.push(
-    <div className = "container">
-        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/bedTop.png"/>
-        <div className = "imageText">10</div>
-    </div>
-        );
-
-const things = barList.map(item => <li className = "thingBar" style ={{listStyleType:'none'}}>{item}</li>)
-
-
-interface Piece{
-    image: string | null
-    x: number
-    y: number
-}
-
-// initialFloorState.push({image: "./images/assets/chair.png", x:0, y:7})
-// initialFloorState.push({image: "./images/assets/table.png", x:0, y:6})
-// initialFloorState.push({image: "./images/assets/table.png", x:0, y:5})
-
 
 function Room(){
-    // const initialFloorState: Piece[] = [];
+    // let barList: JSX.Element[] = []; 
+    // for 8 x 8 tile room
+    const horizontalAxis = ["a","b","c","d","e","f","g","h"]
+    const verticalAxis = ["1","2","3","4","5","6","7","8"]
 
-    // initialFloorState.push({image: "./images/assets/chair.png", x:0, y:7})
-    // initialFloorState.push({image: "./images/assets/table.png", x:0, y:6})
-    // initialFloorState.push({image: "./images/assets/table.png", x:0, y:5})
+    // connect this to the inventory database
+    // each time saved stuff, the count should be updated accordingly
+    // if the count for an item is zero, all items are used up, delete that entry from database
+    // and it should not be rendered in the inventory bar anymore
+    const barListInitial: JSX.Element[] = []; 
+    const chair = ["./images/assets/chairFront.png","./images/assets/chairLeft.png", "./images/assets/chairBack.png", "./images/assets/chairRight.png"];
+
+    const furniture: (Array<string>)[] = [];
+    furniture.push(chair);
+
+    // for each item in inventory, push it into barList to be rendered in inventory bar
+    // each time push item, also push in its count;
+    const countMap = new Map();
+    countMap.set("chair",2);
+    countMap.set("table",2);
+
+    const [itemCount, setItemCount] = useState(countMap); // itemCount is a map of item in inventory and its amt(session persistent only)
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src = {chair[0]}/>
+            <div className = "imageText">{itemCount.get("chair")}</div>
+        </div>
+        );
+
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/table.png"/>
+            <div className = "imageText">{itemCount.get("table")}</div>
+        </div>
+        );
+
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/toilet.png"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/oven.png"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/plant.png"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/window.png"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/painting1.png"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/painting2.png"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/dog.gif"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/cat.gif"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/couchLeft.png"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/couchRight.png"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    barListInitial.push(
+    <div className = "container">
+        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/fridgeTop.png"/>
+        <div className = "imageText">10</div>
+    </div>
+        );
+
+    barListInitial.push(
+    <div className = "container">
+        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/fridgeBottom.png"/>
+        <div className = "imageText">10</div>
+    </div>
+        );
+
+    barListInitial.push(
+    <div className = "container">
+        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/sink.png"/>
+        <div className = "imageText">10</div>
+    </div>
+        );
+
+    barListInitial.push(
+    <div className = "container">
+        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/toiletSide.png"/>
+        <div className = "imageText">10</div>
+    </div>
+        );
+
+    barListInitial.push(
+    <div className = "container">
+        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/bathTubLeft.png"/>
+        <div className = "imageText">10</div>
+    </div>
+        );
+
+    barListInitial.push(
+    <div className = "container">
+        <img className = "picture" style = {{width:150, height:150}} src="./images/assets/bathTubRight.png"/>
+        <div className = "imageText">10</div>
+    </div>
+        );
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/clock.png"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/carpet.png"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/desk.png"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/bedBottom.png"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    barListInitial.push(
+        <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src="./images/assets/bedTop.png"/>
+            <div className = "imageText">10</div>
+        </div>
+            );
+
+    
+    const [barList, setBarList] = useState<JSX.Element[]>(barListInitial);
+    
+    // itemCount.get("chair")
+    // itemCount.get("table")
+
+
+    
+
+    const things = barList.map(item => <li className = "thingBar" style ={{listStyleType:'none'}}>{item}</li>)
+
+
+    interface Piece{
+        image: string | null
+        x: number
+        y: number
+    }
 
     // start out with two windows by default
-    const [initialFloorState, setFloor] = useState<Piece[]>([{image: "./images/assets/window.png", x:1, y:6},{image: "./images/assets/window.png", x:6, y:6}]);
+    // for initial floor state, if there isnt anything in it already, thats a fresh room
+    // if no item in position in database:
+
+    // else if there is smth in database, for each item in database, add into initial floor state
+    // otherwise, push stuff from position database into initial floor state and render that
+    const roomFurnish: Piece[] = [];
+    roomFurnish.push({image: "./images/assets/window.png", x:1, y:6});
+    roomFurnish.push({image: "./images/assets/window.png", x:6, y:6});
+
+    const [initialFloorState, setFloor] = useState<Piece[]>(roomFurnish);
     // initialFloorState.push({image: "./images/assets/chair.png", x:0, y:7})
 
 // when press onto an item, it will push it onto the first upper left tile
@@ -210,7 +229,7 @@ function Room(){
     function spawnItem(e: React.MouseEvent){
         const elem = e.target as HTMLElement;
         const test = [...initialFloorState];
-        console.log("here");
+        // console.log("here");
         console.log(initialFloorState);
 
         // if(elem.classList.contains("picture")){
@@ -224,9 +243,74 @@ function Room(){
         // return test;
         setFloor(test);
         setPieces(test);
-        // console.log(initialFloorState);
-        console.log("waa");
-        console.log(pieces);
+        
+        
+        let count = [...itemCount];
+        let countArray = [...count.values()]; 
+        const mapped = countArray.map(thing => thing[0]);
+
+        let idx = 0;
+        let curr = "";
+        let amt = 0;
+        let currStr = "";
+
+        // mapping here
+        if (elem.getAttribute('src') == "./images/assets/chairFront.png") {
+            idx = mapped.indexOf("chair");
+            curr = "chair";
+            currStr = "./images/assets/chairFront.png";
+        }
+
+        if ((elem.getAttribute('src')) == "./images/assets/table.png"){
+            idx = mapped.indexOf("table");
+            curr = "table";
+            currStr = "./images/assets/table.png";
+        }
+        
+
+        amt = count[idx][1];
+        amt --;
+
+
+        // fix the idx thing here, need to actually find which spot has the thing
+        let newArr = [...barList];
+        let idx2 = 0;
+
+        for (let i = 0; i < newArr.length; i++){
+            // console.log("yo mam")
+            // console.log(currStr);
+            // console.log("vs");
+            // console.log(newArr[i].props.children[0].props.src);
+
+            if (newArr[i].props.children[0].props.src == (currStr)){
+                idx2 = i;
+                console.log(i + currStr);
+                break;
+            }
+        }
+
+        newArr[idx2] = (
+            <div className = "container">
+            <img className = "picture" style = {{width:150, height:150}} src = {currStr}/>
+            <div className = "imageText">{amt}</div>
+        </div>
+        );
+
+        setBarList(newArr);
+
+        // if ran out of item, dont display it in the bar anymore
+        if (amt == 0){
+            // console.log("we in")
+            // console.log(barList);
+            const filtered = newArr.filter((_,i) => i !== idx2);
+            setBarList(filtered);
+            // console.log(barList)
+        }
+
+        let newmap = new Map(itemCount);
+        newmap.set(curr, amt);
+
+        setItemCount(newmap);
     }
 
 
